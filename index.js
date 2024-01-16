cd325e6469ebe50a73d9cf924e993f99319cd763
Tomasz1982 <64226749+Tomasz1982@users.noreply.github.com>
2024-01-16 20:50:02 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Witajcie!, oto pierwsze Repozytorium w Grupie-1");
+console.log(
+  "Zamysł jest taki, żebyśmy się pobawili nieco, pododawali nowe pliki, potworzyli nowe gałęzie, zrobili merge, code review itd"
+);
+console.log(
+  "Chodzi o proste zmiany w kodzie, ktore pozwolą potestować Git-a i GitLaba"
+);
+console.log("Zaczynamy?");
